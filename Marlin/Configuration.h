e8324b94bcfdc8cc04fa9c5a42289624da6f4441
ganeshmev <35722311+ganeshmev@users.noreply.github.com>
2020-07-16 19:36:11 +0530

    Update config.h
    steps per mm, and lowered feedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -730,14 +730,14 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 504, 504, 504, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 503.937, 503.937, 503.937, 503.937 }
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 50, 30, 50, 45 }
+#define DEFAULT_MAX_FEEDRATE          { 50, 30, 30, 45 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -750,7 +750,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 200, 200, 200, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 50, 50, 50, 10000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
