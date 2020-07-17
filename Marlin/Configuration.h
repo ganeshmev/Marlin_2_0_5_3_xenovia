f371caef15f0c144bbcaaab4d26b0580e06ad6fc
ganeshmev <35722311+ganeshmev@users.noreply.github.com>
2020-07-17 11:52:11 +0530

    iterated Z feedrate- slower
    iterated Z feedrate- slower

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -737,7 +737,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 50, 30, 30, 45 }
+#define DEFAULT_MAX_FEEDRATE          { 50, 30, 20, 45 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -1364,8 +1364,8 @@
 #endif
 
 // Homing speeds (mm/m)
-#define HOMING_FEEDRATE_XY (50*60)
-#define HOMING_FEEDRATE_Z  (50*60)
+#define HOMING_FEEDRATE_XY (20*60)
+#define HOMING_FEEDRATE_Z  (20*60)
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
